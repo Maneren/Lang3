3d3ddc9bf4b4c4148655d0ebab79c50b10881776
Maneren <maneren731@gmail.com>
2026-01-30 17:53:18 +0100

    formatting

--- src/vm/src/error.cpp
+++ src/vm/src/error.cpp
@@ -7,7 +7,6 @@ namespace l3::vm {
 UndefinedVariableError::UndefinedVariableError(const Identifier &id)
     : NameError("variable '{}' not declared", id.get_name()) {}
 
-
 RuntimeError::RuntimeError(const std::string &message)
     : std::runtime_error(message) {}
 
